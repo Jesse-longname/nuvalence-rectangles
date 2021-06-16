f53e74f3fb5798eed1a79983fb1f17141801b7e1
Jesse-Daniel Belleau-Karaskewich <jdbkaraskewich@gmail.com>
2021-06-16 00:52:30 -0400

    Step 5: *Setup index.ts for my own testing/debugging

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+import { RectangleService } from './services/rectangle/rectangle.service';
+import { testCases } from './services/rectangle/rectangle.testcases';
+
+const rectService = new RectangleService();
+const test = testCases.find(x => x.description.includes('is a line'));
+if (test) {
+  const res = rectService.getIntersections(test.rectangleA, test.rectangleB);
+}
